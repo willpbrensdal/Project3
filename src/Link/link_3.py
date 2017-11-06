a103f31aaf1f4d1e7b70120217ff2e53d628eee4
TinniestSquash <brensdalw@live.com>
2017-11-05 22:07:43 -0700

    part 3 added nf

--- src/Link/link_3.py
+++ src/Link/link_3.py
@@ -1,6 +1,5 @@
 '''
 Created on Oct 12, 2016
-
 @author: mwitt_000
 '''
 
@@ -9,7 +8,7 @@ import threading
 
 ## An abstraction of a link between router interfaces
 class Link:
-    
+
     ## creates a link between two objects by looking up and linking node interfaces.
     # @param from_node: node from which data will be transfered
     # @param from_intf_num: number of the interface on that node
@@ -23,22 +22,19 @@ class Link:
         self.to_intf_num = to_intf_num
         self.in_intf = from_node.out_intf_L[from_intf_num]
         self.out_intf = to_node.in_intf_L[to_intf_num]
-        #configure the linking interface MTUs
-        self.in_intf.mtu = mtu
-        self.out_intf.mtu = mtu
-        
-        
+        self.mtu = mtu
+
     ## called when printing the object
     def __str__(self):
         return 'Link %s-%d to %s-%d' % (self.from_node, self.from_intf_num, self.to_node, self.to_intf_num)
-        
+
     ##transmit a packet from the 'from' to the 'to' interface
     def tx_pkt(self):
         pkt_S = self.in_intf.get()
         if pkt_S is None:
             return #return if no packet to transfer
-        if len(pkt_S) > self.out_intf.mtu:
-            print('%s: packet "%s" length greater then link mtu (%d)' % (self, pkt_S, self.out_intf.mtu))
+        if len(pkt_S) > self.mtu:
+            print('%s: packet "%s" length greater then link mtu (%d)' % (self, pkt_S, self.mtu))
             return #return without transmitting if packet too big
         #otherwise transmit the packet
         try:
--- src/Simulation/simulation_3.py
+++ src/Simulation/simulation_3.py
@@ -1,6 +1,5 @@
 '''
 Created on Oct 12, 2016
-
 @author: mwitt_000
 '''
 from src.Network import network_3
@@ -10,43 +9,72 @@ from time import sleep
 
 ##configuration parameters
 router_queue_size = 0 #0 means unlimited
-simulation_time = 1 #give the network sufficient time to transfer all packets before quitting
+simulation_time = 2 #give the network sufficient time to transfer all packets before quitting
+
+# Routing tables
+# List of dictionaries, where each dictionary is the table for a router, key is source, value is output interface
+# 0th Index: Router A, 1st Index: Router B, 2nd Index: Router C, 3rd Index: Router D
+forwarding_tables = [{1:0, 2:1},{1:0},{2:0},{1:0,2:0}]
 
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
 
     #create network nodes
-    client = network_3.Host(1)
-    object_L.append(client)
-    server = network_3.Host(2)
-    object_L.append(server)
-    router_a = network_3.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    client1 = network_3.Host(1)
+    object_L.append(client1)
+    client2 = network_3.Host(2)
+    object_L.append(client2)
+    client3 = network_3.Host(3)
+    object_L.append(client3)
+    client4 = network_3.Host(4)
+    object_L.append(client4)
+    router_a = network_3.Router(name='A', intf_count=2, max_queue_size=router_queue_size, forwarding_table=forwarding_tables[0])
     object_L.append(router_a)
+    router_b = network_3.Router(name='B', intf_count=1, max_queue_size=router_queue_size, forwarding_table=forwarding_tables[1])
+    object_L.append(router_b)
+    router_c = network_3.Router(name='C', intf_count=1, max_queue_size=router_queue_size, forwarding_table=forwarding_tables[2])
+    object_L.append(router_c)
+    router_d = network_3.Router(name='D', intf_count=2, max_queue_size=router_queue_size, forwarding_table=forwarding_tables[3 & 4])
+    object_L.append(router_d)
 
     #create a Link Layer to keep track of links between network nodes
     link_layer = link_3.LinkLayer()
     object_L.append(link_layer)
 
+
     #add all the links
-    link_layer.add_link(link_3.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link_3.Link(router_a, 0, server, 0, 50))
-    
-    
+    link_layer.add_link(link_3.Link(client1, 0, router_a, 0, 50))
+    link_layer.add_link(link_3.Link(client2, 0, router_a, 1, 50))
+    link_layer.add_link(link_3.Link(router_a, 0, router_b, 0, 50))
+    link_layer.add_link(link_3.Link(router_a, 1, router_c, 0, 50))
+    link_layer.add_link(link_3.Link(router_b, 0, router_d, 0, 50))
+    link_layer.add_link(link_3.Link(router_c, 0, router_d, 1, 50))
+    link_layer.add_link(link_3.Link(router_d, 0, client3, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 1, client3, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 0, client4, 0, 50))
+    link_layer.add_link(link_3.Link(router_d, 1, client4, 0, 50))
+
     #start all the objects
     thread_L = []
-    thread_L.append(threading.Thread(name=client.__str__(), target=client.run))
-    thread_L.append(threading.Thread(name=server.__str__(), target=server.run))
+    thread_L.append(threading.Thread(name=client1.__str__(), target=client1.run))
+    thread_L.append(threading.Thread(name=client2.__str__(), target=client2.run))
+    thread_L.append(threading.Thread(name=client3.__str__(), target=client3.run))
+    thread_L.append(threading.Thread(name=client4.__str__(), target=client4.run))
     thread_L.append(threading.Thread(name=router_a.__str__(), target=router_a.run))
-    
+    thread_L.append(threading.Thread(name=router_b.__str__(), target=router_b.run))
+    thread_L.append(threading.Thread(name=router_c.__str__(), target=router_c.run))
+    thread_L.append(threading.Thread(name=router_d.__str__(), target=router_d.run))
+
     thread_L.append(threading.Thread(name="Network", target=link_layer.run))
-    
+
     for t in thread_L:
         t.start()
-    
-    
-    #create some send events    
+
+
+    #create some send events
     for i in range(3):
-        client.udt_send(2, 'Sample data %d' % i)
+        client1.udt_send(3, 'Host_1 data %d' % i)
+        client2.udt_send(3, 'Host_2 data %d' % i)
     
     
     #give the network sufficient time to transfer all packets before quitting
